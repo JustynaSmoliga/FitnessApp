21895097330aad7198e5fc4e8421fe3e50307871
Justyna <justyna.smoliga@gmail.com>
2021-09-19 13:01:02 +0200

    datePickerSlice tests fixed

--- src/features/date-picker/datePickerSlice.test.ts
+++ src/features/date-picker/datePickerSlice.test.ts
@@ -6,7 +6,7 @@ import datePickerReducer, {
 } from "./datePickerSlice";
 
 describe("date picker reducer", () => {
-  const diaryDate = moment("02-01-2021");
+  const diaryDate = "2021-09-14T22:00:00.000+00:00";
 
   const initialState: DatePickerState = {
     diaryDate: diaryDate,
@@ -14,11 +14,15 @@ describe("date picker reducer", () => {
 
   test("should increase date", () => {
     const actual = datePickerReducer(initialState, increment());
-    expect(actual.diaryDate).toEqual(diaryDate.add(1, "days"));
+    expect(actual.diaryDate).toEqual(
+      moment(diaryDate).add(1, "days").toISOString()
+    );
   });
 
   test("should decrease date", () => {
     const actual = datePickerReducer(initialState, decrement());
-    expect(actual.diaryDate).toEqual(diaryDate.subtract(1, "days"));
+    expect(actual.diaryDate).toEqual(
+      moment(diaryDate).subtract(1, "days").toISOString()
+    );
   });
 });
