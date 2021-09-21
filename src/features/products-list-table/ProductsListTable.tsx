8f00e061e242deeb354225a7c974b17c36844fa1
Justyna <justyna.smoliga@gmail.com>
2021-09-21 20:25:20 +0200

    modal window added to delete icon button

--- src/features/products-list-table/ProductsListTable.tsx
+++ src/features/products-list-table/ProductsListTable.tsx
@@ -4,6 +4,15 @@ import LocalDiningOutlinedIcon from "@material-ui/icons/LocalDiningOutlined";
 import DeleteIcon from "@material-ui/icons/Delete";
 import IconButton from "@material-ui/core/IconButton";
 import { useDispatch } from "react-redux";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@material-ui/core";
+import { useState } from "react";
 
 interface ProductListTableProps {
   date: string;
@@ -18,32 +27,58 @@ interface RowProps {
 }
 
 const Row: React.FC<RowProps> = (props) => {
+  const [openDialog, setOpenDialog] = useState(false);
   const dispatch = useDispatch();
 
   const deleteProductHandler = () => {
     dispatch(deleteProduct(props.productId));
   };
 
+  const deleteIconClickHandler = () => {
+    setOpenDialog(true);
+  };
+
+  const disagreeButtonClickHandler = () => {
+    setOpenDialog(false);
+  };
+
   return (
-    <tr className={styles.row}>
-      <td>{props.name}</td>
-      <td>{props.totalCalories}</td>
-      <td className={styles.deleteIconButtonContainer}>
-        {props.quantity}
-        {/* <span style={{ width: "10px" }}></span>
-        <IconButton>
-          <DeleteOutlineOutlinedIcon fontSize="small" />
-        </IconButton> */}
-      </td>
-      <td className={styles.deleteButtonDrawer}>
-        <IconButton
-          style={{ width: "10px", height: "10px" }}
-          onClick={deleteProductHandler}
-        >
-          <DeleteIcon style={{ fontSize: "20px" }} />
-        </IconButton>
-      </td>
-    </tr>
+    <>
+      <tr className={styles.row}>
+        <td>{props.name}</td>
+        <td>{props.totalCalories}</td>
+        <td className={styles.deleteIconButtonContainer}>{props.quantity}</td>
+        <td className={styles.deleteButtonDrawer}>
+          <IconButton
+            style={{ width: "10px", height: "10px" }}
+            onClick={deleteIconClickHandler}
+          >
+            <DeleteIcon style={{ fontSize: "20px" }} />
+          </IconButton>
+        </td>
+      </tr>
+      <Dialog
+        open={openDialog}
+        onClose={disagreeButtonClickHandler}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Delete product"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Do you really want delete this product from meal?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={disagreeButtonClickHandler} color="secondary">
+            Disagree
+          </Button>
+          <Button onClick={deleteProductHandler} color="secondary" autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 };
 
